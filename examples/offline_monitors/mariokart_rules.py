0562e6caaf56da9324b148cf470039086d980073
furrrow <jianyu234@gmail.com>
2025-11-10 17:21:18 -0500

    updated mariokart_rules.py

--- examples/offline_monitors/mariokart_rules.py
+++ examples/offline_monitors/mariokart_rules.py
@@ -3,6 +3,9 @@ import sys
 import glob
 import numpy as np
 import csv
+import os
+import re
+from tqdm import tqdm
 
 surface_dict = {
     "dry_sand"      : 74,
@@ -35,7 +38,7 @@ def read_csv(filename, time_tuple=False):
                 column[h].append(float(v))
     return column
 
-def speed_limit_rule(data):
+def speed_limit_rule(data, speed_limit=900):
     """
     inf robustness means it pass by not being triggered
     neg robustness -> bad.
@@ -49,7 +52,7 @@ def speed_limit_rule(data):
     spec.name = 'speed_limit'
 
     spec.declare_var('kart1_speed', 'float')
-    spec.spec = 'kart1_speed <= 10;'
+    spec.spec = f"kart1_speed <= {speed_limit};"
 
     try:
         spec.parse()
@@ -71,25 +74,29 @@ def water_exposure_rule(data):
     - must log robustness score at each timestep, final robustness score is not a summary.
     - the value is the difference between Gnt and the defined Gnt Threshold
     """
-    spec = rtamt.StlDiscreteTimeSpecification()
-    spec.name = 'collision_detection'
+    spec = rtamt.StlDiscreteTimeSpecification(semantics=rtamt.Semantics.OUTPUT_ROBUSTNESS)
+    spec.name = 'water_exposure'
 
-    spec.declare_var('deep_water', 'bool')
-    spec.declare_var('dry_sand', 'bool')
+    spec.declare_var('surface', 'int')
     # '((surface==8) implies (eventually[0:3](always[0:2](surface==74))));'
-    spec.spec = '((deep_water)implies(eventually[0:3](always[0:2](dry_sand))));'
+    spec.spec = '((surface==8)implies(eventually[0:3](always[0:1](surface==74))));'
 
     try:
         spec.parse()
-        # spec.pastify()
+        spec.pastify()
     except rtamt.RTAMTException as err:
         print(f'RTAMT Exception: {err}')
         sys.exit(1)
-    if "time" not in list(data.keys()):
-        data['time'] = data['step']
-    data['deep_water'] = np.array(data['surface']) == surface_dict['deep_water']
-    data['dry_sand'] = np.array(data['surface']) == surface_dict['dry_sand']
-    robustness_list = spec.evaluate(data)
+    # if "time" not in list(data.keys()):
+    #     data['time'] = data['step']
+    robustness_list = []
+    for i in range(len(data['surface'])):
+        rob = spec.update(i, [
+            ('surface', data['surface'][i]),
+        ])
+        robustness_list.append([i, rob])
+        # robustness_list.append([i, rob==0])
+        # print(f"time {i} robustness {rob:.3f}")
     return robustness_list
 
 def collision_rule(data):
@@ -124,13 +131,68 @@ if __name__ == '__main__':
     csv_folder_path = "/home/jim/Projects/rtamt/mariokart_traces/2025-11-03_22-25-04"
     files = glob.glob(csv_folder_path + "/*.csv")
     files.sort()
-    for filename in files:
-        data = read_csv(filename)
-        speed_result = speed_limit_rule(data)
-        water_result = water_exposure_rule(data)
-        collision_result = collision_rule(data)
-        print("speed limit rule:", speed_result[0:10])
-        print("water exposure rule:", water_result[0:10])
-        print("collision rule:", collision_result[0:10])
+    verbose = False
+    speed_limit = 850
 
-        break
+    speed_limit_matrix = []
+    water_rule_matrix = []
+    collision_rule_matrix = []
+    speed_limit_dict = {"avg_neg_robustness": [], "min_neg_robustness": []}
+    water_rule_dict = {"avg_neg_robustness": [], "min_neg_robustness": []}
+    collision_rule_dict = {"avg_neg_robustness": [], "min_neg_robustness": []}
+    for filename in tqdm(files, disable=verbose):
+        trace_name = os.path.split(filename)[-1]
+        trace_idx = re.search("\d+", os.path.split(filename)[-1]).group()
+        trace_idx = int(trace_idx)
+        data = read_csv(filename)
+        speed_result = np.array(speed_limit_rule(data, speed_limit=speed_limit))
+        water_result = np.array(water_exposure_rule(data))
+        collision_result = np.array(collision_rule(data))
+
+        # check if any timesteps have been skipped
+        if speed_result[:, 0][-1] != len(speed_result[:, 0])-1:
+            print(f"warning: {filename} speed_result total timestep mismatch!")
+        if water_result[:, 0][-1] != len(water_result[:, 0])-1:
+            print(f"warning: {filename} water_result total timestep mismatch!")
+        if collision_result[:, 0][-1] != len(collision_result[:, 0])-1:
+            print(f"warning: {filename} collision_result total timestep mismatch!")
+
+        if verbose:
+            print(f"file {filename}")
+            print("speed limit rule:", speed_result[0:10, 1])
+            print("water exposure rule:", water_result[0:10, 1])
+            print("collision rule:", collision_result[0:10, 1])
+            print()
+        speed_limit_matrix.append(speed_result[:, 1])
+        non_neg_val = (speed_result[:, 1][speed_result[:, 1] < 0])
+        speed_limit_dict['avg_neg_robustness'].append(
+            np.average(non_neg_val) if len(non_neg_val) > 0 else 0)
+        speed_limit_dict['min_neg_robustness'].append(
+            np.min(non_neg_val) if len(non_neg_val) > 0 else 0)
+
+        water_rule_matrix.append(water_result[:, 1])
+        non_neg_val = (water_result[:, 1][water_result[:, 1] < 0])
+        water_rule_dict['avg_neg_robustness'].append(
+            np.average(non_neg_val) if len(non_neg_val) > 0 else 0)
+        water_rule_dict['min_neg_robustness'].append(
+            np.min(non_neg_val) if len(non_neg_val) > 0 else 0)
+
+        collision_rule_matrix.append(collision_result[:, 1])
+        non_neg_val = (collision_result[:, 1][collision_result[:, 1] < 0])
+        collision_rule_dict['avg_neg_robustness'].append(
+            np.average(non_neg_val) if len(non_neg_val) > 0 else 0)
+        collision_rule_dict['min_neg_robustness'].append(
+            np.min(non_neg_val) if len(non_neg_val) > 0 else 0)
+        # for debug purposes
+        # if len(speed_limit_dict['avg_neg_robustness']) > 10:
+        #     break
+
+    print("speed_limit_rule:")
+    print(f"average negative robustness {np.average(speed_limit_dict['avg_neg_robustness']):.1f}, "
+          f"minimal negative robustness {np.min(speed_limit_dict['min_neg_robustness']):.1f}")
+    print("water_rule:")
+    print(f"average negative robustness {np.average(water_rule_dict['avg_neg_robustness']):.1f}, "
+          f"minimal negative robustness {np.min(water_rule_dict['min_neg_robustness']):.1f}")
+    print("collision_rule:")
+    print(f"average negative robustness {np.average(collision_rule_dict['avg_neg_robustness']):.1f}, "
+          f"minimal negative robustness {np.min(collision_rule_dict['min_neg_robustness']):.1f}")
